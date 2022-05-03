a339021334dc647dfd04ea131ecd48894a2fd1af
Vinicius Felipe Tenorio <100444356+viniciusvftt@users.noreply.github.com>
2022-05-02 22:05:01 -0400

    Update index.js

--- Nova pasta (2)/index.js
+++ Nova pasta (2)/index.js
@@ -1,20 +1,17 @@
 $("#btlogin").click(
     function(){
-        login = 
-        {
-                'usuario' : document.getElementById('inputEmail').value,
-                'senha' : document.getElementById('inputPass').value,
-                'tipo_login' : 'site'
-        };
 
-      
 
-        if(login.senha == '1234'){
-            document.getElementById('resultado').innerHTML = '<b>Bem vindo!</b>';
-        } else  { 
-            document.getElementById('resultado').innerHTML = '<b>Usuario ou senha invalidos </b>'
-        
+        $.ajax({
+            type: 'GET',
+            url: 'http://jsonplaceholder.typicode.com/users',
+            sucess:function(method) console.log(...data: any[]): void
+                console.log(data);
+
+            
+
+        },
+        error:function(data){
+
         }
-
-    } 
-    )       
+    })   
